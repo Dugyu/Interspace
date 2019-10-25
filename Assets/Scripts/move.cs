cb8126870dc721f18bd3a51d31e0bf11b191afc2
Guangyu Du <guangyudu@Guangyus-MacBook-Pro.local>
2019-10-25 01:11:53 -0400

    first commit

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        y = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,  1.0f * Mathf.Sin(Time.timeSinceLevelLoad) + y,transform.position.z);
+    }
+}
